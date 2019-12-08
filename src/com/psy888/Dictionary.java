0cee861d8403302108325be76b931258f1b80d45
psy888 <tbilisi.zp.snab@gmail.com>
2019-12-08 21:11:31 +0200

    Initial commit

---
+++ src/com/psy888/Dictionary.java
@@ -0,0 +1,8 @@
+package com.psy888;
+
+import java.util.HashMap;
+
+public class Dictionary {
+    HashMap<String, String> base;
+
+}
---
+++ src/com/psy888/FileRW.java
@@ -0,0 +1,17 @@
+package com.psy888;
+
+import java.util.HashMap;
+
+//File read write
+public class FileRW {
+    private static final String FILE_NAME = "base.txt";
+
+    // todo write to file method
+    public boolean saveBase(HashMap<String,String> base){
+        
+        return true;
+    }
+
+    // todo read from file method
+    // string
+}
